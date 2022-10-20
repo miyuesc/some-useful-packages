413fe57fc8d668b45164fe8ce2dfe6b7a8bcf9f8
MiyueSC <913784771@qq.com>
2022-10-20 22:26:39 +0800

    demos

---
+++ src/pages/components/demos/demo11.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="demo11">
+    <highlight-text text="这是高亮文本" size="24px" /><br />
+    <highlight-text text="这是高亮文本2" size="32px" color="#ea28bc" /><br />
+    <highlight-text text="这是高亮文本3" size="32px" color="#ea28bc" :lighting="false" /><br />
+  </div>
+</template>
+
+<script>
+import HighlightText from "@/components/HighlightText";
+export default {
+  name: "demo11",
+  components: { HighlightText },
+  cnName: "高亮文本"
+};
+</script>
+
+<style scoped></style>
