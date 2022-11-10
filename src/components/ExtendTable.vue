d1ff84421dea5c9d8da0b32621ed5b2f259db8e2
MiyueSC <913784771@qq.com>
2022-11-10 14:49:33 +0800

    1

---
+++ src/components/ExtendTable.vue
@@ -0,0 +1,79 @@
+<script>
+import { Table } from "element-ui";
+import { parseHeight } from "element-ui/packages/table/src/util";
+
+export default {
+  name: "ExtendTable",
+  extends: Table,
+  props: {
+    maxHeight: {
+      type: [String, Number],
+      default: "100%"
+    }
+  },
+  computed: {
+    bodyHeight() {
+      const { headerHeight = 0, bodyHeight, footerHeight = 0 } = this.layout;
+      if (this.height) {
+        return {
+          height: bodyHeight ? bodyHeight + "px" : ""
+        };
+      } else if (this.maxHeight) {
+        const maxHeight = parseHeight(this.maxHeight);
+        if (typeof maxHeight === "number") {
+          return {
+            "max-height": maxHeight - footerHeight - (this.showHeader ? headerHeight : 0) + "px"
+          };
+        } else if (maxHeight.match(/\d+(%|vh)/g)) {
+          return {
+            "max-height": `calc(${maxHeight} - ${footerHeight + this.showHeader ? headerHeight : 0}px)`
+          };
+        }
+      }
+      return {};
+    },
+    fixedBodyHeight() {
+      if (this.height) {
+        return {
+          height: this.layout.fixedBodyHeight ? this.layout.fixedBodyHeight + "px" : ""
+        };
+      } else if (this.maxHeight) {
+        let maxHeight = parseHeight(this.maxHeight);
+        if (typeof maxHeight === "number") {
+          maxHeight = this.layout.scrollX ? maxHeight - this.layout.gutterWidth : maxHeight;
+          if (this.showHeader) {
+            maxHeight -= this.layout.headerHeight;
+          }
+          maxHeight -= this.layout.footerHeight;
+          return {
+            "max-height": maxHeight + "px"
+          };
+        } else if (maxHeight.match(/\d+(vh|%)/g)) {
+          // let resHeight = this.layout.scrollX ? `100% - ${this.layout.gutterWidth}px` : "100%";
+          let resHeight = "100%";
+          if (this.showHeader) {
+            resHeight = `${resHeight} - ${this.layout.headerHeight}px`;
+          }
+          resHeight = `${resHeight} - ${this.layout.footerHeight}px`;
+          return {
+            "max-height": `calc(${resHeight})`
+          };
+        }
+      }
+      return {};
+    }
+  }
+};
+</script>
+<style scoped>
+.el-table {
+  display: flex;
+  flex-direction: column;
+}
+.el-table >>> .el-table__header-wrapper {
+  flex-shrink: 0;
+}
+.el-table >>> .el-table__body-wrapper {
+  flex-grow: 1;
+}
+</style>
---
+++ src/pages/components/demos/demo16.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="demo-page">
+    <extend-table :data="tableData">
+      <el-table-column fixed prop="date" label="日期" width="250"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="220"> </el-table-column>
+      <el-table-column prop="province" label="省份" width="120"> </el-table-column>
+      <el-table-column prop="city" label="市区" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址" width="300"> </el-table-column>
+      <el-table-column fixed="right" prop="zip" label="邮编" width="120"> </el-table-column>
+    </extend-table>
+  </div>
+</template>
+
+<script>
+import ExtendTable from "@/components/ExtendTable";
+export default {
+  name: "demo16",
+  components: { ExtendTable },
+  cnName: "ElTable扩展",
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
