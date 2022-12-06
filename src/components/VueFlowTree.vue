6ea6eb32508ea3cf99403323d0f9cc1a1ca75559
MiyueSC <913784771@qq.com>
2022-12-06 15:27:47 +0800

    1

---
+++ src/components/VueFlowTree.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="VueFlowTree">
+    <h1>VueFlowTree Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueFlowTree"
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/VueFlowTreeItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="VueFlowTreeItem">
+    <h1>VueFlowTreeItem Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueFlowTreeItem"
+};
+</script>
+
+<style scoped></style>
